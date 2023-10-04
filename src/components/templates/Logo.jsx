f664d6fa7ccfb83386f155f6477e8b0f7175b2df
Rafael Magno <rafaelmagno125@yahoo.com.br>
2023-10-04 11:48:53 -0300

    Configuração da logo

--- src/components/templates/Logo.jsx
+++ src/components/templates/Logo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Logo.css";
+import logo from "../../assets/images/logo.png"
 
 
 
 const Logo = (props) => {
     return (
         <aside className="logo">
-            Logo
+            <a href="/" className="logo">
+                <img src={logo} alt="logo" />
+            </a>
         </aside>
     )
 };
